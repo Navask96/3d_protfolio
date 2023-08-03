a1402bd35f8959b068efc35c19b572ac7a7fa629
sasith_debug <sashith@debugsire.com>
2023-08-03 14:30:21 +0530

    3d_protfolio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 3d_protfolio
 My Demo Protfolio.
+ Create With Reat + Vito
+  Clone It And npm run Dev to run this project.
---
+++ src/components/Works.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-no-undef */
+import React from 'react';
+import { github } from '../assets';
+import { SectionWrapper } from "../hoc";
+import { projects } from '../constatnts';
+import { fadeIn} from '../utils/motion';
+import { styles } from "../styles";
+import { motion } from 'framer-motion';
+import { textVariant } from '../utils/motion';
+
+const ProjectCard = ({
+  index,            // Accessing the 'index' prop
+  name,             // Accessing the 'name' prop
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt='project_image'
+            className='object-cover w-full h-full rounded-2xl'
+          />
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='flex items-center justify-center w-10 h-10 rounded-full cursor-pointer black-gradient'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='object-contain w-1/2 h-1/2'
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
+
+        <div className='flex flex-wrap gap-2 mt-4'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+function Works() {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          My Work
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Projects.
+        </h2>
+      </motion.div>
+      <div className='flex w-full'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
+      <div>
+      
+
+      </div>
+
+    </>
+  );
+}
+
+export default SectionWrapper(Works,"");
